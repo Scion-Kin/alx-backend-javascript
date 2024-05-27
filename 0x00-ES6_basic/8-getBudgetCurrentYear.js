6223834e7e6aeffc0d3c0362341d55b3f67a2c73
Scion-Kin <dinturner17@gmail.com>
2024-05-27 18:55:31 +0200

    Task 8 done

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,11 @@
+const getCurrentYear = () => new Date().getFullYear();
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+
+  budget[`income-${getCurrentYear()}`] = income;
+  budget[`gdp-${getCurrentYear()}`] = gdp;
+  budget[`capita-${getCurrentYear()}`] = capita;
+
+  return budget;
+}
