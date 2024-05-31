3954c5bdef83abd5ea31647c94d7df925831d334
Scion-Kin <dinturner17@gmail.com>
2024-05-31 17:54:55 +0200

    Task 5 done

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,6 +1,14 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
+    }
   }
 
   get sqft() {
