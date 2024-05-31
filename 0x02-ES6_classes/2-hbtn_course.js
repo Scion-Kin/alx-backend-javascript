5ae68aaa3f5c4814b219fc22eaaf40a62c304624
Scion-Kin <dinturner17@gmail.com>
2024-05-31 16:57:05 +0200

    Task 2 done

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,12 +1,11 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof (name) !== 'string') throw new TypeError('Name must be a string');
-
-    if (typeof (length) !== 'number') throw new TypeError('Length must be a number');
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
 
     this._name = name;
     this._length = length;
-    this._students = [...students];
+    this._students = students;
   }
 
   get name() {
@@ -22,18 +21,16 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
-    if (typeof (name) !== 'string') throw new TypeError('Name must be a string');
-
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
     this._name = name;
   }
 
   set length(length) {
-    if (typeof (length) !== 'number') throw new TypeError('Length must be a number');
-
+    if (typeof length !== 'number') throw new TypeError('Length must be a number');
     this._length = length;
   }
 
   set students(students) {
-    this._students = [...students];
+    this._students = students;
   }
 }
