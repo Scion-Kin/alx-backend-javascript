eebdb68314819fa43249bf336c4f933161e4c354
Scion-Kin <dinturner17@gmail.com>
2024-05-31 09:21:48 +0200

    Task 2 done.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({ success: 200, body: 'success' }))
-    .catch(() => Error())
+    .catch(() => { throw new Error(); })
     .finally(() => console.log('Got a response from the API'));
 }
