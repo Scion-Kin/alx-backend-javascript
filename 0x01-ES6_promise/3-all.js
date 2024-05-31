87436a21f334ea9ef17eafca6c26ca0c6e93d2f2
Scion-Kin <dinturner17@gmail.com>
2024-05-31 10:11:05 +0200

    Task 4 done.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,8 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default async function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((response) => console.log(response[0].body,
-      response[1].firstName,
-      response[1].lastName))
+    .then((response) => {
+      console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastName}`)
+    })
     .catch(() => console.log('Signup system offline'));
 }
