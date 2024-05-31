f1ca80ee485b189cfa271609c624c0bbcd1a9512
Scion-Kin <dinturner17@gmail.com>
2024-05-31 11:27:14 +0200

    Task 6 done

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,8 @@
-import signUpUser from "./4-user-promise"
-import uploadPhoto from "./5-photo-reject"
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    return Promise
+  return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((response) => (
       response.map((object) => ({
