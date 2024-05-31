97707123da7e9157fd03e02ef0df3243eb038f0e
Scion-Kin <dinturner17@gmail.com>
2024-05-31 11:26:21 +0200

    Task ./com done

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from "./4-user-promise"
+import uploadPhoto from "./5-photo-reject"
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => (
+      response.map((object) => ({
+        status: object.status,
+        value: object.status === 'fulfilled' ? object.value : String(object.reason),
+      }))
+    ));
+}
