ec7e2724b75a82d77bb31a90faa1bb173aebc53b
Scion-Kin <dinturner17@gmail.com>
2024-05-31 17:23:34 +0200

    Task 2 done

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,20 +1,12 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = [...students];
   }
 
   get name() {
     return this._name;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
   }
 
   set name(name) {
@@ -22,12 +14,20 @@ export default class HolbertonCourse {
     this._name = name;
   }
 
+  get length() {
+    return this._length;
+  }
+
   set length(length) {
     if (typeof length !== 'number') throw new TypeError('Length must be a number');
     this._length = length;
   }
 
+  get students() {
+    return this._students;
+  }
+
   set students(students) {
-    this._students = students;
+    this._students = [...students];
   }
 }
