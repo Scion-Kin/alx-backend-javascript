1cb413bfb07580c598875d91deec537078c2ef8b
Scion-Kin <dinturner17@gmail.com>
2024-06-01 21:54:47 +0200

    Task 11 done

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,9 +1,9 @@
-import Car from './10-car'
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range
+    this._range = range;
   }
 
   cloneCar() {
