c64206f5c948d127e3f0d6147b2087cb622a25e9
Scion-Kin <dinturner17@gmail.com>
2024-06-02 13:26:49 +0200

    Task 14 done

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,7 @@
 export default function iterateThroughObject(reportWithIterator) {
   let str = '';
   for (const i of reportWithIterator) {
-    str += ` ${i}`;
+    str += `${i} | `;
   }
-  return str.slice(1).replace(/ /g, ' | ');
+  return str.slice(0, -3);
 }
