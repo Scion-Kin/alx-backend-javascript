6d372cdef1c6d7038162ae505797c0179e143e3f
Scion-Kin <dinturner17@gmail.com>
2024-06-02 13:44:03 +0200

    Task 10 done

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,8 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
+  try {
     return {
-        photo: await uploadPhoto(),
-        user: await createUser()
-    }
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
