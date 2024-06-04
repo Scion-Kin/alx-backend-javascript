81de5634b6d12a19285e43eb5cb4c4b8831616ce
Scion-Kin <dinturner17@gmail.com>
2024-06-04 22:15:19 +0200

    Task 11 done

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,7 @@
 export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
-  if (weakMap.get(endpoint) >= 5) {
+  if (weakMap.has(endpoint) && weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   } else if (!weakMap.has(endpoint)) {
     weakMap.set(endpoint, 1);
