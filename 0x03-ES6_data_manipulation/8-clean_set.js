7f309acd9d06cfb2c8775eb103b8cd21c33f3e67
Scion-Kin <dinturner17@gmail.com>
2024-06-04 21:15:27 +0200

    Task 8 done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,5 @@
 export default function cleanSet(set, str) {
   if (!(set instanceof Set) || typeof str !== 'string' || !str) return '';
 
-  return [...set]
-    .filter((val) => typeof val === 'string' && val.startsWith(str))
-    .map((val) => val.slice(str.length))
-    .join('-');
+  return [...set].filter((val) => typeof val === 'string' && val.startsWith(str)).map((val) => val.slice(str.length)).join('-');
 }
