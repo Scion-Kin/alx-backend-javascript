e57642fbfa6dbd7c09bc2aeeb59ba267c098d455
Scion-Kin <dinturner17@gmail.com>
2024-06-04 20:27:55 +0200

    Task 7 done

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,6 @@
 export default function hasValuesFromArray(set, arr) {
-  for (const i of arr) {
-    if (!set.has(i)) return false
+  for (const i of arr.values()) {
+    if (!set.has(i)) return false;
   }
   return true;
 }
