0192916bf605e1efd1a6aeb405b8ce4904616dcc
Scion-Kin <dinturner17@gmail.com>
2024-07-23 15:17:53 +0200

    Task 0 done

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  console.log(`Your name is ${data.slice(0, -1)}`);
+});
+
+process.on('SIGINT', () => {
+  console.log('This important software is now closing');
+  process.exit(0);
+});
