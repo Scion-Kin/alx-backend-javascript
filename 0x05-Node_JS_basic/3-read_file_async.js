f43d3c554957b7d27317d30aed1360d7d5de57ea
Scion-Kin <dinturner17@gmail.com>
2024-07-26 15:39:05 +0200

    Task 3 done

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,22 +1,22 @@
 const fs = require('fs').promises;
 
 module.exports = async function countStudents(file) {
-    try {
-        const stats = await fs.stat(file);
-        if (!stats.isFile()) {
-            throw new Error('Cannot load the database');
-        }
-        const content = await fs.readFile(file, 'utf-8');
-        const lines = content.trim().split('\n');
-        const students = lines.slice(1, lines.length).map((arg) => arg.split(','));
-        const fields = new Set(students.map((arg) => arg[3]));
-        console.log(`Number of students: ${lines.length - 1}`);
-
-        fields.forEach((field) => {
-            const all = students.filter((stu) => stu[3] === field).map((stu) => stu[0]);
-            console.log(`Number of students in ${field}: ${all.length}. List: ${all.join(', ')}`);
-        });
-    } catch {
-        throw new Error('Cannot load the database');
+  try {
+    const stats = await fs.stat(file);
+    if (!stats.isFile()) {
+      throw new Error('Cannot load the database');
     }
+    const content = await fs.readFile(file, 'utf-8');
+    const lines = content.trim().split('\n');
+    const students = lines.slice(1, lines.length).map((arg) => arg.split(','));
+    const fields = new Set(students.map((arg) => arg[3]));
+    console.log(`Number of students: ${lines.length - 1}`);
+
+    fields.forEach((field) => {
+      const all = students.filter((stu) => stu[3] === field).map((stu) => stu[0]);
+      console.log(`Number of students in ${field}: ${all.length}. List: ${all.join(', ')}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 };
