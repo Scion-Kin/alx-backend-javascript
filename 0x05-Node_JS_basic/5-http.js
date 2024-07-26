e4ac0f1f4e09bdd57d1a01cd28c827f58f911ea8
Scion-Kin <dinturner17@gmail.com>
2024-07-26 16:30:49 +0200

    Task 5 done

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,31 +1,32 @@
 const fs = require('fs').promises;
+
+async function countStudents(file) {
+  try {
+    const stats = await fs.stat(file);
+    if (!stats.isFile()) {
+      throw new Error('Cannot load the database');
+    }
+    const content = await fs.readFile(file, 'utf-8');
+    const lines = content.trim().split('\n');
+    const students = lines.slice(1, lines.length).map((arg) => arg.split(','));
+    const fields = new Set(students.map((arg) => arg[3]));
+    const response = [`Number of students: ${lines.length - 1}`];
+
+    fields.forEach((field) => {
+      const all = students.filter((stu) => stu[3] === field).map((stu) => stu[0]);
+      response.push(`Number of students in ${field}: ${all.length}. List: ${all.join(', ')}`);
+    });
+    return response.join('\n');
+  } catch (error) {
+    return '';
+  }
+}
+
 const app = require('http').createServer((req, res) => {
   if (req.url === '/') {
     res.end('Hello Holberton School!');
-  }
-  else if (req.url === '/students') {
-    async function countStudents(file) {
-        try {
-            const stats = await fs.stat(file);
-            if (!stats.isFile()) {
-              throw new Error('Cannot load the database');
-            }
-            const content = await fs.readFile(file, 'utf-8');
-            const lines = content.trim().split('\n');
-            const students = lines.slice(1, lines.length).map((arg) => arg.split(','));
-            const fields = new Set(students.map((arg) => arg[3]));
-            const response = [`Number of students: ${lines.length - 1}`];
-
-            fields.forEach((field) => {
-              const all = students.filter((stu) => stu[3] === field).map((stu) => stu[0]);
-              response.push(`Number of students in ${field}: ${all.length}. List: ${all.join(', ')}`);
-            });
-            return response.join('\n');
-        } catch (error) {
-            // do nothing
-        }
-    }
-    countStudents(process.argv[2]).then(data => res.end(data));
+  } else if (req.url === '/students') {
+    countStudents(process.argv[2]).then((data) => res.end(data));
   }
 }).listen(1245);
 
