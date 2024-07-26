3bce0e8ea04da5fba3698344bc1a7f79fd87fc55
Scion-Kin <dinturner17@gmail.com>
2024-07-26 17:45:44 +0200

    Task 7 done

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const fs = require('fs').promises;
+
+async function countStudents(file) {
+  try {
+    const stats = await fs.stat(file);
+    if (!stats.isFile()) {
+      throw new Error('Cannot load the database');
+    }
+    const content = await fs.readFile(file, 'utf-8');
+    const lines = content.trim().split('\n');
+    const students = lines.slice(1, lines.length).map((arg) => arg.split(','));
+    const fields = new Set(students.map((arg) => arg[3]));
+    const response = [`Number of students: ${lines.length - 1}`];
+
+    fields.forEach((field) => {
+      const all = students.filter((stu) => stu[3] === field).map((stu) => stu[0]);
+      response.push(`Number of students in ${field}: ${all.length}. List: ${all.join(', ')}`);
+    });
+    return response.join('\n');
+  } catch (error) {
+    console.log(error);
+    return 'Cannot load the database';
+  }
+}
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+    countStudents(process.argv[2])
+      .then((data) => {
+        res.send('This is the list of our students\n' + data);
+      });
+});
+
+app.listen(1245);
+
+module.exports = app;
