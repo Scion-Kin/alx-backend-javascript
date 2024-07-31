f43fb8b4b578cd8e63c20b441781228168ab1e86
Scion-Kin <dinturner17@gmail.com>
2024-07-31 19:46:26 +0200

    Task 8, 9 and 10 done

--- 0x06-unittests_in_js/hbtn.test.js
+++ 0x06-unittests_in_js/hbtn.test.js
@@ -1,25 +1,13 @@
-const assert = require("assert");
-const calculateNumber = require("./0-calcul.js");
+const { expect } = require('chai');
+const request = require('request');
 
-describe('calculateNumber', () => {
-  it('it round the first argument', () => {
-    assert.equal(calculateNumber(1.0, 0), 1);
-    assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(1.7, 0), 2);
-  });
+describe('Index page', () => {
+  const endpoint = 'http://localhost:7865';
 
-  it('it round the second argument', () => {
-    assert.equal(calculateNumber(0, 1.0), 1);
-    assert.equal(calculateNumber(0, 1.3), 1);
-    assert.equal(calculateNumber(0, 1.7), 2);
-  });
-
-  it('it should return the right number', () => {
-    assert.equal(calculateNumber(1.3, 0), 1);
-    assert.equal(calculateNumber(0, 1.2), 1);
-    assert.equal(calculateNumber(1.3, 1.3), 2);
-    assert.equal(calculateNumber(1.7, 1.2), 3);
-    assert.equal(calculateNumber(1.3, 1.8), 3);
-    assert.equal(calculateNumber(1.6, 1.8), 4);
+  it('Returns the right status', function (done) {
+    request(endpoint, function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
   });
 });
